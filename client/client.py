ebfabce5fb731da2f0249ef7e69bfad3d1df3756
1-moon <hhm9124@gmail.com>
2025-03-17 01:20:07 +0900

    complete client server

--- client/client.py
+++ client/client.py
@@ -1,89 +1,49 @@
+
+# network in client sides 
+
 import sys
-from PyQt5.QtWidgets import *
-from PyQt5.QtCore import *
-from PyQt5.QtGui import *
-from PyQt5 import uic
+import os 
+sys.path.append(os.path.abspath(os.path.join(os.path.dirname(__file__), '..')))
+from config import SERVER_PORT, SERVER_IP
+
 from PyQt5.QtNetwork import QTcpSocket
-from config import SERVER_HOST, SERVER_PORT
+from PyQt5.QtCore import QObject, pyqtSignal 
 
-from_class = uic.loadUiType("./client.ui")[0]
-
-class MainWindow(QMainWindow, from_class):
+class SocketClient(QObject) :
+    # event를 받을 수 있는 signals 
+    receive_data = pyqtSignal(str)
+    connected = pyqtSignal()
+    disconnected = pyqtSignal()
+    error = pyqtSignal(str)
+    
     def __init__(self):
         super().__init__()
-        self.setupUi(self)
-
-        self.setWindowTitle("User")
+        self.socket = QTcpSocket()
         
-        self.stackedWidget.setCurrentWidget(self.login_page)
+        # connect socket - event 
+        self.socket.readyRead.connect(self.readData)
+        self.socket.connected.connect(self.on_connected)
+        self.socket.disconnected.connect(self.on_disconnected)
+        self.socket.errorOccurred.connect(self.on_error)
 
-        # init socket 
+    def connect(self):
+        self.socket.connectToHost(SERVER_IP, SERVER_PORT)
+    def readData(self):
+        data = self.socket.readAll().data().decode()
+        self.receive_data.emit(data)
+    def sendData(self, msg):
+        if self.socket.state() == QTcpSocket.ConnectedState:
+            self.socket.write(msg.encode())
+            print(f"[Client] Sent: {msg}")
         
-        # 로그인 화면 버튼 이벤트
-        self.login_btn.clicked.connect(self.login)
-        self.register_btn.clicked.connect(self.register)
-        self.find_id_btn.clicked.connect(self.find_id)
-        self.find_passwd_btn.clicked.connect(self.find_passwd)
+    # socket callbacks : gui에서 받을 수 있음 
+    def on_connected(self):
+        print("[Client] Connected to server")
+        self.connected.emit()
+    def on_disconnected(self):
+        print("[Client] Disconnected from server")
+        self.disconnected.emit()
+    def on_error(self,err):
+        print(f"[Client] Socket error: {err}")
+        self.error.emit(str(err))
 
-        #회원가입 화면 버튼 이벤트
-        self.register_complete_btn.clicked.connect(self.register_complete)
-
-        #아이디 찾기 화면 버튼 이벤트
-        self.find_id_result.clicked.connect(self.id_check)
-
-        #비밀번호 찾기 화면 버튼 이벤트
-        self.id_check_btn.clicked.connect(self.id_check)
-
-        #비밀번호 재설정 화면 버튼 이벤트
-        self.passwd_reset_btn.clicked.connect(self.passwd_reset)
-
-    def login(self):
-        self.stackedWidget.setCurrentWidget(self.login_page)
-    
-    def register(self):
-        self.stackedWidget.setCurrentWidget(self.register_page)
-    
-    def find_id(self):
-        self.stackedWidget.setCurrentWidget(self.find_id_page)
-    
-    def find_passwd(self):
-        self.stackedWidget.setCurrentWidget(self.id_check_page)
-    
-    def register_complete(self):
-        self.stackedWidget.setCurrentWidget(self.login_page)
-    
-    def id_check(self):
-        current_widget = self.stackedWidget.currentWidget()
-        widget_name = current_widget.objectName()
-        #print(widget_name)
-        if widget_name == "find_id_page":
-            name = self.find_id_input_name.text()
-            email = self.find_id_input_email.text()
-            cur.execute("SELECT login_id FROM user where name = %s and email = %s",(name,email))
-            rows = cur.fetchall()
-            if len(rows) == 0:
-                QMessageBox.about(self, "아이디 찾기", "일치하는 정보가 없습니다.")
-            else:
-                QMessageBox.about(self, "아이디 찾기", f"아이디는 {rows[0][0]}입니다.")
-                self.stackedWidget.setCurrentWidget(self.login_page)
-        else:
-            name = self.id_check_input_name.text()
-            id = self.id_check_input_id.text()
-            email = self.id_check_input_email.text()
-            cur.execute("SELECT login_id FROM user where name = %s and email = %s and login_id = %s",(name,email,id))
-            rows = cur.fetchall()
-            if len(rows) == 0:
-                QMessageBox.about(self, "아이디 확인", "일치하는 아이디가 없습니다.")
-            else:
-                QMessageBox.about(self, "아이디 확인", f"아이디가 확인되었습니다.")
-                self.stackedWidget.setCurrentWidget(self.passwd_reset_page)
-    
-    def passwd_reset(self):
-        self.passwd_reset_input_passwd.text()
-        self.stackedWidget.setCurrentWidget(self.login_page)
-
-if __name__ == "__main__":
-    app = QApplication(sys.argv)
-    client = MainWindow()
-    client.show()
-    sys.exit(app.exec_())
--- server/server.py
+++ server/server.py
@@ -16,7 +16,7 @@ class FAAServer(QTcpServer):
         super(FAAServer, self).__init__()
 
     # client 연결 기다리기 
-    def server_listen(self):
+    def listen(self):
         if self.listen(QHostAddress.Any, SERVER_PORT): print(f"Server listening on port {SERVER_PORT}")
         else: print(f"Failed to listen on port {SERVER_PORT}")
     
@@ -25,13 +25,12 @@ class FAAServer(QTcpServer):
         client_socket = ClientHandler(socketDescrpiter)
         client_socket.start() # thread 시작 
     
-
 if __name__ == "__main__":
     app = QCoreApplication(sys.argv)
     
     conn = FAAdb()          # db연결  
     server = FAAServer()    
-    server.server_listen()   
+    server.listen()   
 
 
 
