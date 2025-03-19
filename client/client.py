012c3726b3c1e5f78ec2561acc1e797ee6db5950
serin <qkrtpfls9902@gmail.com>
2025-03-19 19:40:49 +0900

    client-server

--- client/client.py
+++ client/client.py
@@ -7,9 +7,9 @@ sys.path.append(os.path.abspath(os.path.join(os.path.dirname(__file__), '..')))
 from config import SERVER_PORT, SERVER_IP
 
 from PyQt5.QtNetwork import QTcpSocket
-from PyQt5.QtCore import QObject, pyqtSignal 
+from PyQt5.QtCore import QObject, pyqtSignal,QCoreApplication
 
-class SocketClient(QObject) :
+class Client(QObject) :
     # event를 받을 수 있는 signals 
     receive_data = pyqtSignal(str)
     connected = pyqtSignal()
@@ -21,25 +21,39 @@ class SocketClient(QObject) :
         self.socket = QTcpSocket()
         
         # connect socket - event 
-        self.socket.readyRead.connect(self.readData)
-        self.socket.connected.connect(self.on_connected)
-        self.socket.disconnected.connect(self.on_disconnected)
-        self.socket.errorOccurred.connect(self.on_error)
+        # self.socket.readyRead.connect(self.readData)
+        # self.socket.connected.connect(self.on_connected)
+        # self.socket.disconnected.connect(self.on_disconnected)
+        # self.socket.errorOccurred.connect(self.on_error)
 
     def connect(self):
         self.socket.connectToHost(SERVER_IP, SERVER_PORT)
+        print("연결성공!")
+        self.send_data()
+
+    def send_data(self):
+        if self.socket.state() == QTcpSocket.ConnectedState:  # :흰색_확인_표시: 연결 상태 확인
+            message = b"hi,server"
+            self.socket.write(message)
+            self.socket.flush()
+
     def readData(self):
-        data = self.socket.readAll().data().decode()
-        self.receive_data.emit(data)
+        if self.socket.bytesAvailable()>0:
+            response = self.socket.readAll().data()
+            print("서버 응답 : ",response.decode("utf-8"))
+            # self.receive_data.emit(data)
+
     def sendData(self, msg):
         if self.socket.state() == QTcpSocket.ConnectedState:
-            self.socket.write(msg.encode())
-            print(f"[Client] Sent: {msg}")
+            message = b"hi,server"
+            self.socket.write(message)
+            print("서버로 메세지 전송 : ", message.decode("utf-8"))
         
     # socket callbacks : gui에서 받을 수 있음 
-    def on_connected(self):
-        print("[Client] Connected to server")
-        self.connected.emit()
+    # def on_connected(self):
+    #     print("[Client] Connected to server")
+    #     self.connected.emit()
+
     def on_disconnected(self):
         print("[Client] Disconnected from server")
         self.disconnected.emit()
@@ -47,3 +61,7 @@ class SocketClient(QObject) :
         print(f"[Client] Socket error: {err}")
         self.error.emit(str(err))
 
+if __name__ == "__main__":
+    app = QCoreApplication(sys.argv)
+    client = Client()
+    sys.exit(app.exec_())
--- server/client_handler.py
+++ server/client_handler.py
@@ -1,39 +1,33 @@
-
-
-# Server의 각 client와의 연결 관리 
-# 소켓을 다루고 client로 부터 받은 데이터 처리하고 응답을 보내는 코드 
-
 from PyQt5.QtCore import QThread
 from PyQt5.QtNetwork import QTcpSocket
-
 class ClientHandler(QThread):
     def __init__(self, socketDescriptor):
+        super().__init__()
+        self.socketDescriptor = socketDescriptor
+        self.client_socket = None
+    def run(self):
+        """
+        QThread가 start()될 때 실제로 동작하는 메인 함수.
+        여기서 소켓을 생성하고, 이벤트(readyRead 등)를 연결한 뒤
+        self.exec_()로 이벤트 루프를 돌려주어야 신호 처리가 정상적으로 이루어짐짐
+        """
         self.client_socket = QTcpSocket()
-        self.client_socket.setSocketDescriptor(socketDescriptor)
-
-    def start(self):
+        # 소켓 디스크립터를 현재 쓰레드의 소켓에 세팅
+        self.client_socket.setSocketDescriptor(self.socketDescriptor)
         # 클라이언트로부터 데이터가 도착했을 때
         self.client_socket.readyRead.connect(self.receive_data)
         # 클라이언트가 연결을 끊었을 때
         self.client_socket.disconnected.connect(self.disconnected)
-        self.client_socket.errorOccurred.connect(lambda err: self.socketError(self.client_socket, err))
-
-    def socketError(self, client_socket, err):
-        if client_socket.state() == QTcpSocket.UnconnectedState: self.clientDisconnected(client_socket)
-
+        # 쓰레드 내 이벤트 루프 시작
+        self.exec_()
     def receive_data(self):
-        data = self.client_socket.readAll().data()
-        if data:
-            print("Data received from client.") 
-            self.send_response("Data received successfully")
-
+        while self.client_socket.bytesAvailable() > 0:
+            data = self.client_socket.readAll().data()
+            print("[Server] 클라이언트 메세지 : ", data.decode("utf-8"))
+            response = b"Server Received Your Message"
+            self.client_socket.write(response)
+            self.client_socket.flush()
     def disconnected(self):
-        # 클라이언트가 연결을 끊었을 때 처리 
-        print(f"client disconnected : {self.client_socket}")
-        self.client_socket.deleteLater()
-        self.quit()  # 스레드 종료
-
-    def send_response(self, message):
-        # 클라이언트에 응답 보내기
-        self.client_socket.write(message.encode())
-        print("Response sent to client.")
+        print(f"[Server] client disconnected : {self.client_socket.peerAddress().toString()}")
+        self.client_socket.close()
+        self.quit()  # 스레드 이벤트 루프 종료
